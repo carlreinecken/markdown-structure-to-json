2cf2b13f3d3f4341f98625e04a532c2a1d26174c
Carl Reinecken <carl@reinecken.net>
2024-12-16 12:40:06 +0100

    update dev dependencies

--- dist/markdown-structure-to-json.js
+++ dist/markdown-structure-to-json.js
@@ -1,8 +1,8 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
-  (global = global || self, global['markdown-structure-to-json'] = factory());
-}(this, (function () { 'use strict';
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global["markdown-structure-to-json"] = factory());
+})(this, (function () { 'use strict';
 
   var parser = (text, options) => {
     let HEADER_REGEX = null;
@@ -184,4 +184,4 @@
 
   return parser;
 
-})));
+}));
